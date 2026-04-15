5ad1a0c88a4c0d96ae67615bf5ea97db4b9344cb
nobody <nobody@example.com>
2026-04-15 16:52:54 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -19,7 +21,15 @@ public class Main {
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
 
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStream inputStream = clientSocket.getInputStream();
+
+            while (true) {
+                byte[] b = new byte[1024];
+                int cnt = inputStream.read(b);
+                String s = new String(b);
+                outputStream.write("+PONG\r\n".getBytes());
+            }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
