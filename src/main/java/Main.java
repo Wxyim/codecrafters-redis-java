3d5b9289ab8e4f3261f44faeb128c15e0c075318
nobody <nobody@example.com>
2026-04-25 20:05:25 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,14 +32,16 @@ public class Main {
 
         Socket mainSocket = null;
 
-        Map<String, Object> replMap = new HashMap<>();
-
         try {
           serverSocket = new ServerSocket(port);
           // Since the tester restarts your program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
+
+            Map<String, String> replMap = new ConcurrentHashMap<>();
+
+            Map<String, Date> replMapTime = new ConcurrentHashMap<>();
 
 
           if (argsMap.containsKey("replicaof")) {
@@ -56,7 +58,36 @@ public class Main {
                           String message;
                           while ((message = bufferedReader.readLine()) != null) {
                               if (message.startsWith("*")) {
+                                  int length = Integer.parseInt(message.substring(1));
+                                  if (length > 0) {
+                                      List<String> aa = new ArrayList<>();
+                                      for (int i = 0; i < length; i++) {
+                                          int l = Integer.parseInt(bufferedReader.readLine().substring(1));
+                                          if (l == -1) {
+                                              aa.add(null);
+                                              continue;
+                                          }
+                                          String m = bufferedReader.readLine();
+                                          aa.add(m);
+                                      }
 
+                                      for (int i = 0; i < aa.size(); i++) {
+                                          if (aa.get(i).equals("SET")) {
+                                              boolean f = false;
+                                              if (i + 3 < aa.size()
+                                                      && (aa.get(i + 3).equalsIgnoreCase("px")
+                                                      || aa.get(i + 3).equalsIgnoreCase("ex"))) {
+                                                  Date date = aa.get(i + 3).equalsIgnoreCase("px")
+                                                          ? new Date(System.currentTimeMillis() + Long.parseLong(aa.get(i + 4)))
+                                                          : new Date(System.currentTimeMillis() + Long.parseLong(aa.get(i + 4)) * 1000);
+                                                  replMapTime.put(aa.get(i + 1), date);
+                                              }
+                                              replMap.put(aa.get(i + 1), aa.get(i + 2));
+                                              printWriter.print("+OK" + "\r\n");
+                                              printWriter.flush();
+                                          }
+                                      }
+                                  }
                               } else if (message.startsWith("+PONG")) {
                                   if (repl) {
                                       printWriter.print("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + port + "\r\n");
