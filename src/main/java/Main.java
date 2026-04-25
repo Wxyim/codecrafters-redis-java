f5d29d75cc5177bd920539d20226a50068a6465c
nobody <nobody@example.com>
2026-04-25 19:47:42 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -921,7 +921,7 @@ public class Main {
                                         } else if (aa.get(i).equals("PSYNC")) {
                                             printWriter.print("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n");
                                             printWriter.flush();
-                                            printWriter.print("$120\r\nUkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==");
+                                            printWriter.print("$176\r\n524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2");
                                             printWriter.flush();
                                         }
 
