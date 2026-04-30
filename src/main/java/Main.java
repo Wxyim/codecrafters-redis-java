6299d221fbed06c3604a44f29ef03d5024045d48
nobody <nobody@example.com>
2026-04-30 21:04:52 +0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1611,7 +1611,8 @@ public class Main {
                                                 if (username.equalsIgnoreCase("default")) {
                                                     printWriter.print("*2\r\n" +
                                                             "$5\r\nflags\r\n" +
-                                                            "*0\r\n");
+                                                            "*1\r\n" +
+                                                            "$6\r\nnopass\r\n");
                                                     printWriter.flush();
                                                 }
                                             }
